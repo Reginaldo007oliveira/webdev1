331c54a191079d5cd1b509feee66f7f64255f387
Reginaldo Oliveira <86331339+Reginaldo007oliveira@users.noreply.github.com>
2021-08-03 10:47:56 -0300

    Add files via upload

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id: number;
+  productName: string;
+  price: number;
+  image: string;
+  amount: number;
+}
+
+export interface Stock {
+  id: number;
+  amount: number;
+}
